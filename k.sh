63e17b5888adc42c8b5995a2e74778bb87980f96
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-07-30 20:50:39 +0530

    Update k.sh

--- k.sh
+++ k.sh
@@ -5,7 +5,9 @@ echo -e "\e[4;32m for password separator press 1"
 echo 'for updating press 2 '
 read -p "enter option" k
 if [ "$k"==1 ]
+then
 python password.py
 
 elif [ "$k"==2 ]
+then
 bash u.sh
