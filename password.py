7645489ecbc1cfa3e56586a2840dd0903de9e524
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-07-30 20:29:28 +0530

    Update password.py

--- password.py
+++ password.py
@@ -6,9 +6,9 @@ os.system('bash k.sh')
 
 
 print("file should in working directory ")
-print('for updating press 1 / else enter the name of file')
+print('for updating press u / else enter the name of file')
 
-l=str(input("Enter file name to seperate password from file "))
+l=input("Enter file name to seperate password from file ")
 if l!='':
         
     k=open(l,'r')
@@ -18,7 +18,7 @@ if l!='':
         h=k.readline()
         g=h.split(":")
         print(g[1])
-elif l==str(1):
+elif l==u or l==U :
         os.system('bash u.sh')
 else:
     print('\033[5;31m error occured invalid input')
