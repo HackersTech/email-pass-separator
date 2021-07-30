7d3a28b10fc38cb73c99d51b21b17bfe56e7d9eb
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-07-30 21:30:15 +0530

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,9 +1,9 @@
 clear
 bash m.sh |lolcat
 
-echo -e "\e[4;32m for password separator press 1"
-echo 'for updating press 2 '
-read -p "enter option" k
+echo -e "\e[4;32m 1) for password separator press 1"
+echo '2)for updating press 2 '
+read -p "enter option-:" k
 if [ "$k"==1 ]
 then
 python password.py
