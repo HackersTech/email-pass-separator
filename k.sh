e18e1a4362fee793818766163539932ce5a30f69
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-07-30 20:50:14 +0530

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,2 +1,11 @@
 clear
 bash m.sh |lolcat
+
+echo -e "\e[4;32m for password separator press 1"
+echo 'for updating press 2 '
+read -p "enter option" k
+if [ "$k"==1 ]
+python password.py
+
+elif [ "$k"==2 ]
+bash u.sh
