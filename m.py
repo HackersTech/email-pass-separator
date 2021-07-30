47f4725a6ebb24aa0eb24fe53fd5bdc524eeae21
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-07-30 19:22:54 +0530

    Add files via upload

---
+++ m.py
@@ -0,0 +1,25 @@
+import os
+print("\033[5;32m insta id hackers__tech")
+print("\033[5;33m link of youtube channel in repo")
+
+    
+
+
+print("file should in working directory ")
+
+
+l=input("Enter file name to seperate password from file ")
+if l!='':
+        
+    k=open(l,'r')
+
+    h=' '
+    while h:
+        h=k.readline()
+        g=h.split(":")
+        print(g[1])
+
+else:
+    print('\033[5;31m error occured invalid input')
+    print("\033[5;35m starting again")
+    os.system("python3 password.py")
