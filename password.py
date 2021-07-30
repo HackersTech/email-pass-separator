6275092e6e449a360e7553f18e59130eb89f4e0a
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-07-30 20:43:41 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,12 +1,8 @@
 import os
 
 print("\033[5;33m link of youtube channel in repo")
-os.system('bash k.sh')
 
 
-
-print("file should in working directory ")
-print('for updating press u / else enter the name of file')
 
 l=input("Enter file name to seperate password from file ")
 if l!='':
@@ -20,8 +16,7 @@ if l!='':
                         print(g[1])
     except:
                 k.close()
-elif l=='u' or l=='U' :
-        os.system('bash u.sh')
+
 else:
     print('\033[5;31m error occured invalid input')
     print("\033[5;35m starting again")
