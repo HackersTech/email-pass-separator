744355f8225b5fa8f0e622162c52a9bbe8776cdb
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-07-30 21:43:55 +0530

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,5 +1,8 @@
 cd ..
+echo "updating ---" lolcat 
 rm -rf email-pass-separator
 git clone https://github.com/HackersTech/email-pass-separator
 cd email-pass-separator
+echo "updated ||||||||||||||" | lolcat
+sleep 0.3
 bash k.sh
