c68f5a4c17d744328fde99baaffed2636df8c376
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-07-31 17:32:29 +0530

    Update k.sh

--- k.sh
+++ k.sh
@@ -2,7 +2,7 @@ clear
 bash m.sh |lolcat
 chmod +x k.sh 
 
-echo -e "\e[5;32m 1) for password separator press 1"
+echo -e "\e[4;32m 1) for password separator press 1"
 echo '2)for updating press 2 '
 read -p "enter option-:" k
 if [ "$k" == 1 ]
